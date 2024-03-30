64f2c6f69905bbd645d4a14cda852a035bfc6467
ArturVayutskiy <vayutskiy@gmail.com>
2024-03-30 09:41:18 +0200

    save

---
+++ src/js/1-timer.js
@@ -0,0 +1,19 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const input = document.querySelector('#datetime-picker');
+const startButton = document.querySelector('button');
+const dates = document.querySelectorAll('.value');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(selector, options);
+let userSelectedDate;
